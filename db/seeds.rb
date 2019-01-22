32f370deebf16dfe8bd72a9ad51202b31e516a90
github username <github email address>
2019-01-21 21:20:52 -0500

    added more lectures to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@ Ash = User.find_or_create_by(username: "Ash", first_name: "Ashley", last_name: "
 JC = User.find_or_create_by(username: "jerryjcchang", first_name: "JC", last_name: "Chang", mod: 3)
 
 Lecture34 = Video.find_or_create_by(name: "Lecture 34 - Rails API - Part 1", description: "Rails API - Part 1", instructor: "Ann", url: "https://www.youtube.com/embed/DBX9eWBU1O4", length: 44, category: "Mod 3")
+Lecture34_2 = Video.find_or_create_by(name: "Lecture 34 - Rails API - Part 2", description: "Rails API - Part 2", instructor: "Ann", url: "https://www.youtube.com/embed/9PZ8CPi0Ct8", length: 48, category: "Mod 3")
+ToyReview = Video.find_or_create_by(name: "Toy Tale Lab Review - Part 1", description: "Review of Toy Tale Lab", instructor: "Will", url: "https://www.youtube.com/embed/q7uR5rfWQKo", length: 32, category: "Mod 3")
+ToyReview_2 = Video.find_or_create_by(name: "Toy Tale Lab Review - Part 2", description: "Review of Toy Tale Lab - Part 2", instructor: "Will", url: "https://www.youtube.com/embed/aj4VhM3ydao", length: 49, category: "Mod 3")
+ToyReview_3 = Video.find_or_create_by(name: "Toy Tale Lab Review - Part 3", description: "Review of Toy Tale Lab - Part 3", instructor: "Will", url: "https://www.youtube.com/embed/lLN1jj0vD20", length: 26, category: "Mod 3")
