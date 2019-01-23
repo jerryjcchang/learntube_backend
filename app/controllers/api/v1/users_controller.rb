2fda7d2f747257644b03a24a6e75d1aef3514294
github username <github email address>
2019-01-23 18:56:20 -0500

    end of class wed commit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,20 @@ class Api::V1::UsersController < ApplicationController
 	  render json: User.find(params[:id])
 	end
 
-	
+	def update
+		User.find(params[:id]).update(params)
+		render json: User.find(params[:id])
+	end
+
+	def add_video
+		User.find(params[:user_id]).videos << Video.find(params[:video_id])
+		render json: User.find(params[:user_id]).videos.last
+	end
+
+	private
+	def strong_params
+		params.require(:user).permit(:name, :username, :first_name,
+		 :last_name, :status)
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, :videos
+      resources :users, :videos
+      patch '/users/:user_id/videos/add', to: 'users#add_video'
     end
   end
 end
