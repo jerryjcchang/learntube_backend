e4b59334970eda434f7aa43ba80f9e5323a9910d
Ash Zaki <ashleya.zaki@gmail.com>
2019-01-23 20:35:55 -0500

    ...

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,24 @@ class Api::V1::UsersController < ApplicationController
 	  render json: User.find(params[:id])
 	end
 
-	
+	def create
+	  render json: User.create(strong_params)
+	end
+
+	def update
+		User.find(params[:id]).update(strong_params)
+		render json: User.find(params[:id])
+	end
+
+	def destroy
+		render json: User.find(params[:id]).destroy
+	end
+
+	private
+
+	def strong_params
+		params.require(:user).permit(:name, :username, :first_name,
+		 :last_name, :status)
+	end
 
 end
--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -14,10 +14,16 @@ class Api::V1::VideosController < ApplicationController
 	  render json: Video.create(strong_params)
 	end
 
+	def update
+		Video.find(params[:id]).update(strong_params)
+		render json: Video.find(params[:id])
+	end
+
+
 	private
 
 	def strong_params
 		params.require(:video).permit(:name, :description, :instructor,
-		 :length, :category, :youtube_id)
+		 :length, :category, :youtube_id,)
 	end
 end
